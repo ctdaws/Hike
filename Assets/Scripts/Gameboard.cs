f11fe091cf461cffb08203be542749eed58bbefb
Chris Dawson <chrisdawson1996@gmail.com>
2022-02-03 18:00:31 +0000

    Added capability to move a selected card to a tile on the gameboard

--- Assets/Scripts/Gameboard.cs
+++ Assets/Scripts/Gameboard.cs
@@ -4,17 +4,57 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class Gameboard : MonoBehaviour {
+    private Tilemap tilemap = null;
     private Grid grid = null;
-    public Tilemap tilemap = null;
-    public Tile hoverTile = null;
+
+    // Tilemap bottom left cell position in cell coordinates
+    private Vector3Int tilemapPosition;
+    // Tilemap size in cell coordinates
+    private Vector3Int tilemapSize;
+
+    public GameObject card = null;
+    private SelectCard selectCard;
 
     void Start() {
-        grid = gameObject.GetComponent<Grid>();
+        tilemap = gameObject.GetComponent<Tilemap>();
+        grid = tilemap.layoutGrid;
+        tilemapPosition = tilemap.cellBounds.position;
+        tilemapSize = tilemap.cellBounds.size;
+        selectCard = card.GetComponent<SelectCard>();
+        Debug.Log(grid.cellSize);
     }
 
-    void Update() {
-        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3Int tileCoordinate = grid.WorldToCell(mouseWorldPos);
-        tilemap.SetTile(tileCoordinate, hoverTile);
+    void OnMouseDown() {
+        if (selectCard.isSelected) {
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int tileCell = grid.WorldToCell(mouseWorldPos);
+
+            if (canPlaceCardAtCell(tileCell)) {
+                float cellHeight = grid.cellSize.y;
+                // This line is assuming 2x1 card in vertical position
+                card.transform.position = (tilemap.GetCellCenterWorld(tileCell) + new Vector3(0f, -(cellHeight / 2), 0f));
+            }
+        }
+    }
+
+    bool isCellInBounds(Vector3Int tileCell) {
+        if (
+            tileCell.x >= tilemapPosition.x &&
+            tileCell.x < (tilemapPosition.x + tilemapSize.x) &&
+            tileCell.y >= tilemapPosition.y &&
+            tileCell.y < (tilemapPosition.y + tilemapSize.y)
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // This function assumes a card is 2x1 tiles and the tile under the cursor is the top tile of the card
+    bool canPlaceCardAtCell(Vector3Int tileCell) {
+        if (isCellInBounds(tileCell)) {
+            return tileCell.y > tilemapPosition.y;
+        }
+        return false;
     }
 }
--- Assets/Scripts/SelectCard.cs
+++ Assets/Scripts/SelectCard.cs
@@ -4,28 +4,19 @@ using UnityEngine;
 
 public class SelectCard : MonoBehaviour
 {
-    bool isSelected;
-    
-    // Start is called before the first frame update
+    public bool isSelected;
+
     void Start()
     {
         isSelected = false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void OnMouseDown() {
         if (!isSelected) {
             isSelected = true;
-            Debug.Log("Card selected");
         }
         else if (isSelected) {
             isSelected = false;
-            Debug.Log("Card deselected");
         }
     }
 }
