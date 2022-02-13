78e8981b99d81ed3d0bada06fe5d65bf8f951528
Chris Dawson <chrisdawson1996@gmail.com>
2022-02-13 17:08:40 +0000

    Add basic hand and omnisharp config file

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour {
+    public GameObject[] cards;
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+}
